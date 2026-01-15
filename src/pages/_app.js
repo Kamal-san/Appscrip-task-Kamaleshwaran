0bb82ac5f40275c5d5e33ef750308fc258faf351
Kamal-san <kamaleshw046@gmail.com>
2026-01-16 02:30:34 +0530

    Modified _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,7 @@
 import "../styles/globals.css";
-import "../styles/home.css";
+import "../styles/Home.css";
 //import "../styles/header.css";
-import "../styles/footer.css";
+import "../styles/Footer.css";
 import "../styles/Filters.css";
 import "../styles/Layout.css";
 import "../styles/mobile.css";
